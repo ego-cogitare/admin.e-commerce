0b4b25200ecee1d2e8b09767bde646d0aa06f2e0
Alexander Bogish <ego.cogitare@gmail.com>
2017-10-22 12:14:09 +0300

    ...

--- src/actions/Message.js
+++
@@ -1,7 +0,0 @@
-import { request } from '../core/helpers/Request';
-export function clearStream(courseId, success, error) {
- request(`/message/delete/channel/${courseId}`, {}, 'delete',
-    (r) => success,
-    (e) => error
-  );
-}
